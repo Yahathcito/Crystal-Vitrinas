13ccdf48f2095e4cde9654d8edc2e31c3ea00acb
Yahath <yahathhernandezarrieta@gmail.com>
2026-02-26 02:02:47 -0600

    se agregan nuevas vitrinas

--- vitrinas/data-vitrinas.js
+++ vitrinas/data-vitrinas.js
@@ -3,7 +3,7 @@ const VITRINAS = {
     nombre: "Vitrina Caja",
     descripcion: "Mueble para caja registradora.",
     images: [
-      "../img/muebleCaja.jpg",
+      "../img/MuebleCaja copy.jpg",
       "../img/MuebleCaja(1).png",
       "../img/MuebleCaja(2).png",
       "../img/MuebleCaja(3).png",
@@ -17,72 +17,147 @@ const VITRINAS = {
       x Ancho: 60cm
       x Profundidad: 45cm`,
       //pon emojis para cada especificación
-      "✅ Acabado en aluminio blanco",
+      "✅ Acabado en aluminio silver",
       "✅ Grosor del vidrio: 4mm",
       "✅ Rodines incorporados para fácil movilidad",
       "✅ Base de PVC protegido de vidrio",
       "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
-      "✅ Extras: Luz en tira led o linterna en parte superior",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
       "📏 Se fabrica a la medida y gusto del cliente"
     ]   
   },
-  vitrina2: {
-    nombre: "Vitrina Confitera",
-    descripcion: "Diseño compacto para espacios reducidos.",
-    // lista de imágenes que se muestran en la galería (la primera es la principal)
-    images: [
-      "../img/confitera (1).webp",
-      "../img/confitera (1).webp", // duplicado de ejemplo; reemplazar por otra foto real
-    ],
-    especificaciones: [
-      "Vidrio reforzado",
-      "Estructura metálica",
-      "Color: Plateado",
-      "Dimensiones: 50cm alto x 60cm largo x 35cm fondo"
-    ]
-  },
 
-  vitrina3: {
-    nombre: "Vitrina De Torre",
-    descripcion: "Diseño compacto para espacios reducidos.",
+  vitrina2: { 
+    nombre: "Vitrina Torre",
+    descripcion: "Vitrina de torre con acabado en aluminio.",
     images: [
-      "../img/Torre 180 alto x 60cm ancho x35cm de fondo.webp",
-      // añadir más fotos aquí
+      "../img/TorreBronce.jpg",
+      "../img/TorreBronce1.png",
+      "../img/TorreBronce2.png",
+      "../img/TorreBronce3.png",
+      "../img/TorreBronce4.png",
     ],
-    especificaciones: [
-      "Vidrio reforzado",
-      "Estructura metálica",
-      "Color: plateado con azulejo claro",
-      "Dimensiones: Torre 180 alto x 60cm ancho x35cm de fondo"
-    ]
+        especificaciones: [
+      "📏Medidas Alto: 175cm x Ancho: 60cm x Profundidad: 40cm",
+      "✅ Acabado en aluminio bronce",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ]  
   },
-
-    vitrina4: {
-    nombre: "Vitrina Comercial Grande",
-    descripcion: "Vitrina ideal para negocios con alto flujo de clientes.",
+  vitrina3: { 
+    nombre: "Vitrina Pequeña Silver",
+    descripcion: "Vitrina pequeña con acabado en aluminio silver.",
     images: [
-      "../img/2m largo x1.05m alto x45cm ancho.webp",
-      // fotos adicionales opcionales
+      "../img/PequeñaSilver.jpg",
+      "../img/PequeñaSilver1.png",
+      "../img/PequeñaSilver2.png",
+      "../img/PequñeaSilver3.png",
+      "../img/PequeñaSilver4.png",
+      "../img/PequeñaSilver5.png"
     ],
-    especificaciones: [
-      "Vidrio reforzado",
-      "Estructura metálica",
-      "Color plateado con azulejo claro",
-      "Dimensiones: 2m largo x 1.05m alto x45cm ancho"
-    ]
+        especificaciones: [
+      "📏Medidas Alto: 105cm x Ancho: 50cm x Profundidad: 40cm",
+      "✅ Acabado en aluminio silver",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ] 
   },
-
-      vitrina5: {
-    nombre: "Vitrina Comercial mediana",
-    descripcion: "Diseño comercial para flujo de clientes.",
+  vitrina4: { 
+    nombre: "Vitrina Grande Silver",
+    descripcion: "Vitrina grande con acabado en aluminio silver.",
     images: [
-      "../img/1.50cm largo x 1.05 alto x 45cm ancho.webp",
+      "../img/GrandeSilver.jpg",
+      "../img/GrandeSilver1.png",
+      "../img/GrandeSilver2.png",
+      "../img/GrandeSilver3.png",
+      "../img/GrandeSilver4.png",
+      "../img/GrandeSilver5.png"
     ],
-    especificaciones: [
-      "Vidrio reforzado",
-      "Estructura metálica",
-      "Color plateado con azulejo claro",
-      "Dimensiones: 1.50cm largo x 1.05 alto x 45cm ancho"
-    ]
+        especificaciones: [
+      "📏Medidas Alto: 105cm x Ancho: 2m x Profundidad: 45cm",
+      "✅ Acabado en aluminio bronce",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ] 
+  },
+  vitrina5: { 
+    nombre: "Vitrina Pequeña Negra",
+    descripcion: "Vitrina pequeña con acabado en aluminio negro.",
+    images: [
+      "../img/PequeñaNegra.jpg", 
+      "../img/PequeñaNegra1.png", 
+      "../img/PequeñaNegra2.png", 
+      "../img/PequeñaNegra3.png", 
+      "../img/PequeñaNegra4.png", 
+      "../img/PequeñaNegra5.png"
+    ],
+        especificaciones: [
+      "📏Medidas Alto: 105cm x Ancho: 45cm x Profundidad: 40cm",
+      "✅ Acabado en aluminio negro",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Llavín incorporado para mayor seguridad",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ] 
+  },
+  vitrina6: { 
+    nombre: "Vitrina Torre Silver Cuadrada",  
+    descripcion: "Vitrina torre cuadrada con acabado en aluminio silver.",
+    images: [
+        "../img/TorreSilverCuadrada.jpg",  
+        "../img/TorreSilverCuadrada1.png",  
+        "../img/TorreSilverCuadrada2.png",  
+        "../img/TorreSilverCuadrada3.png",  
+        "../img/TorreSilverCuadrada4.png",  
+        "../img/TorreSilverCuadrada5.png"  
+     ],
+        especificaciones: [
+      "📏Medidas Alto: 175cm x Ancho: 60cm x Profundidad: 40cm",
+      "✅ Acabado en aluminio silver",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ] 
+  },
+  vitrina7: { 
+    nombre: "Vitrina Torre Mediana",  
+    descripcion: "Vitrina torre mediana con acabado en aluminio silver.",
+    images: [
+        "../img/TorreMediana.jpg",  
+        "../img/TorreMediana1.png",  
+        "../img/TorreMediana2.png",  
+        "../img/TorreMediana3.png",  
+        "../img/TorreMediana4.png",  
+        "../img/TorreMediana5.png"  
+     ],
+        especificaciones: [
+      "📏Medidas Alto: 170cm x Ancho: 40cm x Profundidad: 40cm",
+      "✅ Acabado en aluminio Negro",
+      "✅ Grosor del vidrio: 4mm",
+      "✅ Rodines incorporados para fácil movilidad",
+      "✅ Base de PVC protegido de vidrio",
+      "✅ Llavín incorporado para mayor seguridad",
+      "✅ Ofrecemos diferentes colores del aluminio: negro,bronce y silver",
+      "➕ Extras: Luz en tira led o linterna en parte superior",
+      "📏 Se fabrica a la medida y gusto del cliente"
+    ] 
   }
 };
